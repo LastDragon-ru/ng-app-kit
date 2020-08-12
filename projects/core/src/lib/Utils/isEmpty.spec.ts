63045abaa3abc899f938f37a9b11a08400188eaf
Aleksei Lebedev <LastDragon@yandex.ru>
2020-08-12 19:11:03 +0400

    `isEmpty()` supports `Node` and `ElementRef<T extends Node>` + tests.

---
+++ projects/core/src/lib/Utils/isEmpty.spec.ts
@@ -0,0 +1,100 @@
+import {ElementRef} from '@angular/core';
+import {Properties} from 'projects/core/src/lib/classes/Properties';
+import {isEmpty}    from 'projects/core/src/lib/Utils/isEmpty';
+import {using}      from 'projects/core/src/lib/Utils/Tests/using';
+
+describe('isEmpty', () => {
+    const dataProvider: Properties<{ value: any; empty: boolean; error?: true }> = {
+        'null':                    {
+            value: null,
+            empty: true,
+        },
+        'true':                   {
+            value: true,
+            empty: false,
+        },
+        'false':                   {
+            value: false,
+            empty: false,
+        },
+        'undefined':               {
+            value: undefined,
+            empty: true,
+        },
+        '0':                       {
+            value: 0,
+            empty: false,
+        },
+        'empty string':            {
+            value: '',
+            empty: true,
+        },
+        'spaces':                  {
+            value: '  ',
+            empty: true,
+        },
+        'string':                  {
+            value: '123',
+            empty: false,
+        },
+        'empty array':             {
+            value: [],
+            empty: true,
+        },
+        'array':                   {
+            value: [1],
+            empty: false,
+        },
+        'empty object':            {
+            value: {},
+            empty: true,
+        },
+        'object':                  {
+            value: {a: 123},
+            empty: false,
+        },
+        'html with comment':       {
+            value: (new DOMParser()).parseFromString(' <!-- comment --> ', 'text/html').body,
+            empty: true,
+        },
+        'html with empty element': {
+            value: (new DOMParser()).parseFromString(' <span></span> ', 'text/html').body,
+            empty: false,
+        },
+        'html with element':       {
+            value: (new DOMParser()).parseFromString(' <span>123</span> ', 'text/html').body,
+            empty: false,
+        },
+        'html with entity':        {
+            value: (new DOMParser()).parseFromString('<body> &#x1F981; </body>', 'text/html').body,
+            empty: false,
+        },
+        'ElementRef with comment':       {
+            value: new ElementRef((new DOMParser()).parseFromString(' <!-- comment --> ', 'text/html').body),
+            empty: true,
+        },
+        'ElementRef with empty element': {
+            value: new ElementRef((new DOMParser()).parseFromString(' <span></span> ', 'text/html').body),
+            empty: false,
+        },
+        'ElementRef with element':       {
+            value: new ElementRef((new DOMParser()).parseFromString(' <span>123</span> ', 'text/html').body),
+            empty: false,
+        },
+        'unsupported type':        {
+            value: new Date(),
+            empty: false,
+            error: true,
+        },
+    };
+
+    using(dataProvider, (description, data) => {
+        it(description, () => {
+            if (data.error) {
+                expect(() => isEmpty(data.value)).toThrowError('Unsupported type.');
+            } else {
+                expect(isEmpty(data.value)).toEqual(data.empty);
+            }
+        });
+    });
+});
--- projects/core/src/lib/Utils/isEmpty.ts
+++ projects/core/src/lib/Utils/isEmpty.ts
@@ -1,9 +1,41 @@
-import {isArray} from 'projects/core/src/lib/Utils/isArray';
-import {isObject} from 'projects/core/src/lib/Utils/isObject';
-import {isString} from 'projects/core/src/lib/Utils/isString';
+import {ElementRef} from '@angular/core';
+import {isArray}    from 'projects/core/src/lib/Utils/isArray';
+import {isBoolean}  from 'projects/core/src/lib/Utils/isBoolean';
+import {isNumber}   from 'projects/core/src/lib/Utils/isNumber';
+import {isObject}   from 'projects/core/src/lib/Utils/isObject';
+import {isString}   from 'projects/core/src/lib/Utils/isString';
 
 export function isEmpty(value: any): boolean {
-    return value == null
-        || ((isString(value) || isArray(value)) && value.length === 0)
-        || (isObject(value) && Object.keys(value).length === 0 && value.constructor === Object);
+    if (value === null || value === undefined) {
+        return true;
+    } else if (isNumber(value) || isBoolean(value)) {
+        return false;
+    } else if (isArray(value)) {
+        return value.length === 0;
+    } else if (isString(value)) {
+        return value.trim().length === 0;
+    } else if (isObject(value, Object)) {
+        return Object.keys(value).length === 0;
+    } else if (value instanceof Node) {
+        return isNodeEmpty(value);
+    } else if (value instanceof ElementRef) {
+        return isEmpty(value.nativeElement);
+    } else {
+        // empty
+    }
+
+    throw new Error('Unsupported type.');
+}
+
+function isNodeEmpty(node: Node): boolean {
+    for (let i = 0; i < node.childNodes.length; i++) {
+        const child = node.childNodes.item(i);
+        const type  = child.nodeType;
+
+        if (type === Node.ELEMENT_NODE || (type === Node.TEXT_NODE && !isEmpty(child.nodeValue))) {
+            return false;
+        }
+    }
+
+    return true;
 }
