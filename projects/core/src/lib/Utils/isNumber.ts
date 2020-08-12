8602a417bd19c083626a0f6900e1a4f27978803c
Aleksei Lebedev <LastDragon@yandex.ru>
2020-08-12 19:10:27 +0400

    `isNumber()` will not check Infinity.

--- projects/core/src/lib/Utils/isNumber.ts
+++ projects/core/src/lib/Utils/isNumber.ts
@@ -1,5 +1,3 @@
 export function isNumber(value: any): value is number {
-    return typeof value === 'number'
-        && !Number.isNaN(value)
-        && !Number.isFinite(value);
+    return typeof value === 'number';
 }
