e22598211fc9d042c363f3f71e517210b0597d84
Aleksei Lebedev <LastDragon@yandex.ru>
2020-09-27 16:32:10 +0400

    [forms] New package.

---
+++ projects/forms/src/lib/FormsModule.ts
@@ -0,0 +1,14 @@
+import {CommonModule}        from '@angular/common';
+import {NgModule}            from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+    imports:      [
+        CommonModule,
+        ReactiveFormsModule,
+    ],
+    declarations: [],
+    exports:      [],
+})
+export class FormsModule {
+}
---
+++ projects/forms/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of forms
+ */
+
+export {FormsModule as AppKitFormsModule} from 'projects/forms/src/lib/FormsModule';
