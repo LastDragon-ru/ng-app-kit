6977542b2919f511eb15f360fbbe252b97e74432
Aleksei Lebedev <LastDragon@yandex.ru>
2020-09-28 21:53:08 +0400

    [core] Public API updated and module name fix.

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -2,4 +2,44 @@
  * Public API Surface of core
  */
 
-export {CoreModule as AppKitCoreModule} from 'projects/core/src/lib/CoreModule';
+import {AppError}                from './lib/Classes/AppError';
+import {HashMap}                 from './lib/Classes/HashMap';
+import {ErrorComponent}          from './lib/Components/Error/ErrorComponent';
+import {ErrorFormatter}          from './lib/Components/Error/ErrorFormatter';
+import {ErrorFormattersProvider} from './lib/Components/Error/ErrorFormattersProvider';
+import {ErrorFormatterComponent} from './lib/Components/Error/Formatters/ErrorFormatterComponent';
+import {StatefulComponent}       from './lib/Components/StatefulComponent';
+import {CoreModule}              from './lib/CoreModule';
+import {NotificatorService}      from './lib/Services/NotificatorService';
+import {isArray}                 from './lib/Utils/isArray';
+import {isBoolean}               from './lib/Utils/isBoolean';
+import {isEmpty}                 from './lib/Utils/isEmpty';
+import {isEqual}                 from './lib/Utils/isEqual';
+import {isFunction}              from './lib/Utils/isFunction';
+import {isHashMap}               from './lib/Utils/isHashMap';
+import {isNumber}                from './lib/Utils/isNumber';
+import {isString}                from './lib/Utils/isString';
+import {isUndefined}             from './lib/Utils/isUndefined';
+import {substitute}              from './lib/Utils/substitute';
+
+export {
+    CoreModule as AppKitCoreModule,
+    HashMap,
+    StatefulComponent,
+    AppError,
+    ErrorComponent,
+    ErrorFormatter,
+    ErrorFormattersProvider,
+    ErrorFormatterComponent,
+    NotificatorService,
+    isArray,
+    isBoolean,
+    isEmpty,
+    isEqual,
+    isFunction,
+    isHashMap,
+    isNumber,
+    isString,
+    isUndefined,
+    substitute,
+};
