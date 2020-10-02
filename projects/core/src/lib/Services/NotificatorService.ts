e218a1de46f8c6c2f7f2505277ffbe63e403b8fe
Aleksei Lebedev <LastDragon@yandex.ru>
2020-10-02 15:18:58 +0400

    [core] `NotificatorService` converted into abstract class that will be used as injection token.

--- projects/core/src/lib/Services/NotificatorService.ts
+++ projects/core/src/lib/Services/NotificatorService.ts
@@ -1,28 +1,8 @@
-import {Injectable} from '@angular/core';
-import {AppError}   from '../Classes/AppError';
-import {Template}   from '../Classes/Template';
+import {AppError} from '../Classes/AppError';
+import {Template} from '../Classes/Template';
 
-@Injectable({
-    providedIn: 'root',
-})
-export class NotificatorService {
-    public constructor() {
-        // empty
-    }
+export abstract class NotificatorService {
+    public abstract success<T>(message?: Template<T> | null): void;
 
-    // <editor-fold desc="API">
-    // =========================================================================
-    public success<T>(message: Template<T> | null = null): void {
-        console.log(message);
-    }
-
-    public error<T>(error: Template<T> | AppError | null = null): void {
-        if (error === false) {
-            return;
-        }
-
-        console.error(error);
-    }
-
-    // </editor-fold>
+    public abstract error<T>(error?: Template<T> | AppError | null): void;
 }
