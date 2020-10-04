2b2b86da668206ee362a0a1e4f691e9301ba4105
Aleksei Lebedev <LastDragon@yandex.ru>
2020-10-04 13:28:06 +0400

    [core] `Provider` will accept `deps: [[Token, new Optional()]]`.

--- projects/core/src/lib/Classes/Provider/Tokenized.ts
+++ projects/core/src/lib/Classes/Provider/Tokenized.ts
@@ -1,5 +1,8 @@
-import {Token} from './Token';
+import {Optional, Self, SkipSelf} from '@angular/core';
+import {Token}                    from './Token';
 
 export type Tokenized<T extends unknown[]> = {
-    [P in keyof T]: Token<T[P] extends Array<infer U> ? U : T[P]>
+    [P in keyof T]: Token<Extract<T[P]>> | [Token<Extract<T[P]>>, ...(Optional|SkipSelf|Self)[]]
 };
+
+type Extract<T> = T extends Array<infer U> ? U : T;
