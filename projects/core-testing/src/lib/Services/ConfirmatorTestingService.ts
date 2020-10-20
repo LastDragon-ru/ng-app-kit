94a82667a9b22969b2f92446eecd600f728b5f1b
Aleksei Lebedev <LastDragon@yandex.ru>
2020-10-20 16:20:31 +0400

    [core-testing] Updated interfaces of `ConfirmatorTestingService` and `NotificatorTestingService` (`Severity` support).

--- projects/core-testing/src/lib/Services/ConfirmatorTestingService.ts
+++ projects/core-testing/src/lib/Services/ConfirmatorTestingService.ts
@@ -1,12 +1,16 @@
-import {ConfirmatorService, Template} from '@lastdragon-ru/ng-app-kit-core';
-import {Observable, of}               from 'rxjs';
+import {
+    ConfirmatorService,
+    Severity,
+    Template,
+}                       from '@lastdragon-ru/ng-app-kit-core';
+import {Observable, of} from 'rxjs';
 
 export class ConfirmatorTestingService extends ConfirmatorService {
     public constructor(protected result: boolean) {
         super();
     }
 
-    public confirm<T>(message?: Template<T> | null): Observable<boolean> {
+    public confirm<T>(message?: Template<T> | null, severity?: Severity | null): Observable<boolean> {
         return of(this.result);
     }
 }
--- projects/core-testing/src/lib/Services/NotificatorTestingService.ts
+++ projects/core-testing/src/lib/Services/NotificatorTestingService.ts
@@ -1,11 +1,12 @@
 import {
     AppError,
     NotificatorService,
+    Severity,
     Template,
 } from '@lastdragon-ru/ng-app-kit-core';
 
 export class NotificatorTestingService extends NotificatorService {
-    public error<T>(error?: Template<T> | AppError | null): void {
+    public error<T>(error?: Template<T> | AppError | null, severity?: Severity | null): void {
         // empty
     }
 
